a456e6e0134dc3f8a2f711ac31c86cdd8b9c5eff
nguyenvankien1008 <nguyenkien10042004@gmail.com>
2026-04-08 01:03:39 +0700

    Calculate functions for export

---
+++ Lesson21/calculate.js
@@ -0,0 +1,22 @@
+export function tong(a, b) {
+    return a + b;
+}
+
+export function hieu(a, b) {
+    return a - b;
+}
+
+export function tich(a, b) {
+    return a * b;
+}
+
+export function thuong(a, b) {
+    return a / b;
+}
+
+export default function thongBao() {
+    return "Thông báo ...";
+}
+
+// export { tong as to, hieu as hi, tich as ti, thuong as th }; // đổi tên trong file export
+
