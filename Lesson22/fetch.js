731cf0d873b7207b3b2228148bca13d674cb7435
nguyenvankien1008 <nguyenkien10042004@gmail.com>
2026-04-09 22:55:05 +0700

    Pause Lesson23 at 30p

--- Lesson22/fetch.js
+++ Lesson22/fetch.js
@@ -1,3 +1,19 @@
+const fetchApi = (url) => {
+    const result = fetch(url)
+        .then(respone => respone.json())
+        .then(data => {
+            return data;
+        });
+    return result; 
+    
+}
+
+fetchApi("https://dummyjson.com/products/categories")
+    .then(data => console.log(data))
+
+
+// Get Category
+
 fetch("https://dummyjson.com/products/categories")
     .then(response => response.json())
     .then(data => {
@@ -16,3 +32,28 @@ fetch("https://dummyjson.com/products/categories")
         divCategory.innerHTML = htmls;
         
     })
+
+// End get Category
+
+// Get Products
+
+fetch("https://dummyjson.com/products")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.products);
+        let htmls  = "";
+        data.products.forEach(item => {
+            htmls += `
+                <div class="product-item">
+                    <img src="${item.thumbnail}" alt="${item.title}">
+                    <h3>${item.title}</h3>
+                    <p>${item.price}$</p>
+                </div>
+            `;
+        });
+
+        const divProducts = document.querySelector("#products");
+        divProducts.innerHTML = htmls;
+    })
+
+// End get Products
