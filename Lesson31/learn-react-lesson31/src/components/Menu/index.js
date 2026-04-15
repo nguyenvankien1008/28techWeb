b8dfd09745224b1830af85e73b40e5571b7ac347
nguyenvankien1008 <nguyenkien10042004@gmail.com>
2026-04-15 17:24:58 +0700

    Menu is child component of Header

---
+++ Lesson31/learn-react-lesson31/src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { MenuContext } from "../Layout";
+
+function Menu() {
+    const menus =  useContext(MenuContext);
+
+    console.log(menus);
+    
+    return (
+        <>
+            <ul>
+                {menus.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </>
+    )
+}
+
+export default Menu;
