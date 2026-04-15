e656c69b99d285c0b537700f40383be37b6219bc
nguyenvankien1008 <nguyenkien10042004@gmail.com>
2026-04-15 15:12:02 +0700

    Data files for project

---
+++ Lesson29/learn-react-lesson29/src/data/products.js
@@ -0,0 +1,20 @@
+export const productList = [
+    {
+        id: 1,
+        image: "https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp",
+        name: "Sản phẩm 1",
+        price: "20.000đ",
+    },
+    {
+        id: 2,
+        image: "https://cdn.dummyjson.com/product-images/beauty/eyeshadow-palette-with-mirror/1.webp",
+        name: "Sản phẩm 2",
+        price: "40.000đ",
+    },
+    {
+        id: 3,
+        image: "https://cdn.dummyjson.com/product-images/beauty/powder-canister/1.webp",
+        name: "Sản phẩm 3",
+        price: "60.000đ",
+    },
+]
