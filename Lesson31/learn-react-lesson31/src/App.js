51bccdef457e28ab750c16792ae06498e63bc76e
nguyenvankien1008 <nguyenkien10042004@gmail.com>
2026-04-15 17:24:08 +0700

    Location where import components

---
+++ Lesson31/learn-react-lesson31/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// import Header from './components/Header';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+
+      <Layout />
+    </>
+  );
+}
+
+export default App;
