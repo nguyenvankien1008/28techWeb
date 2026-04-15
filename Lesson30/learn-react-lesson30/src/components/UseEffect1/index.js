fdef446c49c01ede0663e074e95d12f74b0dac25
nguyenvankien1008 <nguyenkien10042004@gmail.com>
2026-04-15 17:20:13 +0700

    Learn useEffect have callback function but don't have dependencies

---
+++ Lesson30/learn-react-lesson30/src/components/UseEffect1/index.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+function UseEffect1() {
+    
+    useEffect(() => {
+        let listItem = document.querySelectorAll("ul li");
+        console.log(listItem);
+    });
+
+    return (
+        <>
+            <ul>
+                <li>Mục 1</li>
+                <li>Mục 2</li>
+                <li>Mục 3</li>
+            </ul>
+        </>
+    )
+}
+
+export default UseEffect1;
