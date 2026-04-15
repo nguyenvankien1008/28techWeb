4f5231194745a3e4b9f718eda743f18e6143ada5
nguyenvankien1008 <nguyenkien10042004@gmail.com>
2026-04-15 15:18:52 +0700

    Location where import react components

---
+++ Lesson29/learn-react-lesson29/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+// import Cart from './components/Cart';
+import Modal from './components/Modal';
+// import Login from "./components/Login";
+// import Menu from "./components/Menu";
+// import ProductList from './components/ProductList';
+// import Country from './components/Country';
+// import Lamp from './components/Lamp';
+
+function App() {
+  return (
+    <>
+      {/* <Login /> */}
+
+      {/* <Menu /> */}
+
+      {/* <ProductList /> */}
+
+      {/* <Country /> */}
+
+      {/* <Lamp /> */}
+
+      {/* <Cart /> */}
+
+      <Modal />
+    </>
+  );
+}
+
+export default App;
