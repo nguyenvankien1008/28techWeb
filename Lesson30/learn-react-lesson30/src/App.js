fc93889a73578df0f7ed6d0bfba2ae8fa55fa718
nguyenvankien1008 <nguyenkien10042004@gmail.com>
2026-04-15 17:18:58 +0700

    Location where import components

---
+++ Lesson30/learn-react-lesson30/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Tabs from './components/Tabs';
+// import UseEffect1 from './components/UseEffect1';
+// import UseEffect2 from './components/UseEffect2';
+// import UseEffect3 from './components/UseEffect3';
+
+function App() {
+  return (
+    <>
+      {/* <UseEffect1 /> */}
+
+      {/* <UseEffect2 /> */}
+
+      {/* <UseEffect3 /> */}
+
+      <Tabs />
+    </>
+  );
+}
+
+export default App;
