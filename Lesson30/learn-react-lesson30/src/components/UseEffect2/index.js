e805cb7ea709e1b74ffbe12dd95910d7f790051a
nguyenvankien1008 <nguyenkien10042004@gmail.com>
2026-04-15 17:20:48 +0700

    Learn useEffect have callback function, have dependencies but dependencies are empty

---
+++ Lesson30/learn-react-lesson30/src/components/UseEffect2/index.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import "./Product.scss";
+
+function UseEffect2() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dummyjson.com/products")
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data.products);
+                setData(data.products);
+            })
+    }, []);
+
+    console.log(data);
+    
+
+    return (
+        <>
+            
+            <div className="product__list">
+                {data.map(item => (
+                    <div className="product__item" key={item.id}>
+                        <div className="product__image">
+                            <img src={item.thumbnail} alt={item.title}/>
+                        </div>
+                        <h3 className="product__title">
+                            {item.title}
+                        </h3>
+                        <div className="product__price">
+                            {item.price}$
+                        </div>
+                    </div>
+                ))}
+                
+            </div>
+        </>
+    )
+}
+
+export default UseEffect2;
