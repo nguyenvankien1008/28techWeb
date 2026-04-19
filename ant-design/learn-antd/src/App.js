32c2eaa8facf75d49bd99ab629c9c8a2be67f51c
nguyenvankien1008 <nguyenkien10042004@gmail.com>
2026-04-19 13:07:15 +0700

    Location where import components

---
+++ ant-design/learn-antd/src/App.js
@@ -0,0 +1,20 @@
+// import { useState } from 'react';
+import './App.css';
+// import LearnButton from './components/LearnButton';
+import LearnGrid from './components/LearnGrid';
+// import { Button, Typography } from "antd";
+// import { ExpandAltOutlined } from "@ant-design/icons";
+
+// const {Title} = Typography;
+
+function App() {
+  return (
+    <>
+      {/* <LearnButton /> */}
+
+      <LearnGrid />
+    </>
+  );
+}
+
+export default App;
